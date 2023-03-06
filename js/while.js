8a2e4cad8092684717d012a0a32736628aee39a7
Miguel Gutierrez <miguel.a.gutierrez87@gmail.com>
2023-03-06 11:22:35 -0600

    Do-while loop exercise

--- js/while.js
+++ js/while.js
@@ -3,7 +3,17 @@
 //    i *= 2;
 //    console.log(i);
 //}
-
+var allCones = Math.floor(Math.random() * 50) + 50;
+console.log('Cones to sell: ' + allCones)
 do {
+    conesSold = Math.floor(Math.random() * 5) + 1;
+    if (conesSold > allCones){
+        console.log('Not enough cones!!');
+        continue;
+    }
+    allCones -= conesSold;
+    console.log('Cones sold: ' + conesSold)
+} while (allCones > 0){
+    console.log('Congrats, you sold all your cones!!');
 
-} while ()
+}
