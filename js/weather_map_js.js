0b338397818ba701f7d8c0f829676e8154a962b4
Miguel Gutierrez <miguel.a.gutierrez87@gmail.com>
2023-04-05 00:15:59 -0500

    Weather Map Project Update

--- js/weather_map_js.js
+++ js/weather_map_js.js
@@ -44,67 +44,77 @@ function displayWeatherData(latitude, longitude) {
         const currentCity = data.city.name;
         $('#currentCity').html(`Current Location: ${currentCity}`);
         //<--------------------------- First Day-------------------------------->
-        const date1 = data.list[2].dt_txt
-        const temperature1 = data.list[2].main.temp.toFixed(1);
-        const weatherDescription1 = data.list[2].weather[0].description;
-        const humidity1 = data.list[2].main.humidity;
-        const wind1 = data.list[2].wind.speed;
-        const pressure1 = data.list[2].main.pressure;
+        const date1 = data.list[1].dt_txt.split(' ')[0];
+        const temperature1 = data.list[1].main.temp.toFixed(1);
+        const weatherCode1 = data.list[1].weather[0].icon;
+        const weatherDescription1 = data.list[1].weather[0].description;
+        const humidity1 = data.list[1].main.humidity;
+        const wind1 = data.list[1].wind.speed;
+        const pressure1 = data.list[1].main.pressure;
         console.log('5 day forecast', data);
         $('#date1').html(`Date: ${date1}`);
         $('#weather1').html(`Temperature: ${temperature1} &deg;F`);
+        $('#weatherCode1').html(`<img src="http://openweathermap.org/img/w/${weatherCode1}.png" >`);
         $('#description1').html(`Weather: ${weatherDescription1}`);
         $('#humidity1').html(`Humidity: ${humidity1} %`);
         $('#wind1').html(`Wind: ${wind1} mph`);
         $('#pressure1').html(`Pressure: ${pressure1} Hg`);
         //<--------------------------- Second Day-------------------------------->
-        const date = data.list[10].dt_txt
-        const temperature = data.list[10].main.temp.toFixed(1);
-        const weatherDescription = data.list[10].weather[0].description;
-        const humidity = data.list[10].main.humidity;
-        const wind = data.list[10].wind.speed;
-        const pressure = data.list[10].main.pressure;
-        $('#date2').html(`Date: ${date}`);
-        $('#weather2').html(`Temperature: ${temperature} &deg;F`);
-        $('#description2').html(`Weather: ${weatherDescription}`);
-        $('#humidity2').html(`Humidity: ${humidity} %`);
-        $('#wind2').html(`Wind: ${wind} mph`);
-        $('#pressure2').html(`Pressure: ${pressure} Hg`);
+        const date2 = data.list[9].dt_txt.split(' ')[0];
+        const temperature2 = data.list[9].main.temp.toFixed(1);
+        const weatherDescription2 = data.list[9].weather[0].description;
+        const weatherCode2 = data.list[9].weather[0].icon;
+        const humidity2 = data.list[9].main.humidity;
+        const wind2 = data.list[9].wind.speed;
+        const pressure2 = data.list[9].main.pressure;
+        $('#date2').html(`Date: ${date2}`);
+        $('#weather2').html(`Temperature: ${temperature2} &deg;F`);
+        $('#weatherCode2').html(`<img src="http://openweathermap.org/img/w/${weatherCode2}.png" >`);
+        $('#description2').html(`Weather: ${weatherDescription2}`);
+        $('#humidity2').html(`Humidity: ${humidity2} %`);
+        $('#wind2').html(`Wind: ${wind2} mph`);
+        $('#pressure2').html(`Pressure: ${pressure2} Hg`);
         //<--------------------------- Third Day-------------------------------->
-        const date3 = data.list[10].dt_txt
-        const temperature3 = data.list[10].main.temp.toFixed(1);
-        const weatherDescription3 = data.list[10].weather[0].description;
-        const humidity3 = data.list[10].main.humidity;
-        const wind3 = data.list[10].wind.speed;
-        const pressure3 = data.list[10].main.pressure;
+        const date3 = data.list[17].dt_txt.split(' ')[0];
+        const temperature3 = data.list[17].main.temp.toFixed(1);
+        const weatherCode3 = data.list[17].weather[0].icon;
+        const weatherDescription3 = data.list[17].weather[0].description;
+        const humidity3 = data.list[17].main.humidity;
+        const wind3 = data.list[17].wind.speed;
+        const pressure3 = data.list[17].main.pressure;
         $('#date3').html(`Date: ${date3}`);
         $('#weather3').html(`Temperature: ${temperature3} &deg;F`);
+        $('#weatherCode3').html(`<img src="http://openweathermap.org/img/w/${weatherCode3}.png" >`);
         $('#description3').html(`Weather: ${weatherDescription3}`);
         $('#humidity3').html(`Humidity: ${humidity3} %`);
         $('#wind3').html(`Wind: ${wind3} mph`);
         $('#pressure3').html(`Pressure: ${pressure3} Hg`);
         //<--------------------------- Fourth Day-------------------------------->
-        const date4 = data.list[10].dt_txt
-        const temperature4 = data.list[10].main.temp.toFixed(1);
-        const weatherDescription4 = data.list[10].weather[0].description;
-        const humidity4 = data.list[10].main.humidity;
-        const wind4 = data.list[10].wind.speed;
-        const pressure4 = data.list[10].main.pressure;
-        $('#date4').html(`Date: ${date}`);
+        const date4 = data.list[25].dt_txt.split(' ')[0];
+        const temperature4 = data.list[25].main.temp.toFixed(1);
+        const weatherCode4 = data.list[25].weather[0].icon;
+        const weatherDescription4 = data.list[25].weather[0].description;
+        const humidity4 = data.list[25].main.humidity;
+        const wind4 = data.list[25].wind.speed;
+        const pressure4 = data.list[25].main.pressure;
+        $('#date4').html(`Date: ${date4}`);
         $('#weather4').html(`Temperature: ${temperature4} &deg;F`);
+        $('#weatherCode4').html(`<img src="http://openweathermap.org/img/w/${weatherCode4}.png" >`);
         $('#description4').html(`Weather: ${weatherDescription4}`);
         $('#humidity4').html(`Humidity: ${humidity4} %`);
         $('#wind4').html(`Wind: ${wind4} mph`);
         $('#pressure4').html(`Pressure: ${pressure4} Hg`);
         //<--------------------------- Fifth Day-------------------------------->
-        const date5 = data.list[10].dt_txt
-        const temperature5 = data.list[10].main.temp.toFixed(1);
-        const weatherDescription5 = data.list[10].weather[0].description;
-        const humidity5 = data.list[10].main.humidity;
-        const wind5 = data.list[10].wind.speed;
-        const pressure5 = data.list[10].main.pressure;
+        const date5 = data.list[33].dt_txt.split(' ')[0];
+        const temperature5 = data.list[33].main.temp.toFixed(1);
+        const weatherCode5 = data.list[33].weather[0].icon;
+        const weatherDescription5 = data.list[33].weather[0].description;
+        const humidity5 = data.list[33].main.humidity;
+        const wind5 = data.list[33].wind.speed;
+        const pressure5 = data.list[33].main.pressure;
         $('#date5').html(`Date: ${date5}`);
         $('#weather5').html(`Temperature: ${temperature5} &deg;F`);
+        $('#weatherCode5').html(`<img src="http://openweathermap.org/img/w/${weatherCode5}.png" >`);
         $('#description5').html(`Weather: ${weatherDescription5}`);
         $('#humidity5').html(`Humidity: ${humidity5} %`);
         $('#wind5').html(`Wind: ${wind5} mph`);
