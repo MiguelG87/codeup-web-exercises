3bf2ef33380d7c1638ae43cce5921f9c017186a9
Miguel Gutierrez <miguel.a.gutierrez87@gmail.com>
2023-04-11 23:41:24 -0500

    Promises Exercise

--- js/promises.js
+++ js/promises.js
@@ -1,9 +1,15 @@
 "use strict";
 
 function dateOfLastCommit(ghUsername) {
-    return fetch('https://api.github.com/users/${ghUsername}/events/public', {
+    return fetch(`https://api.github.com/users/${ghUsername}/events/public`, {
         headers: {
-            'Authorization': 'token' ${GH_API_TOKEN}
+            'Authorization': `token ${GH_API_TOKEN}`
         }
     })
+        .then(response =>response.json())
+        .then(events => {
+            let pushEvents = events.filter(event => event.type === 'PushEvent')
+            return pushEvents[0].created_at
+        });
 }
+dateOfLastCommit("MiguelG87").then(data => console.log(new Date(data)));
